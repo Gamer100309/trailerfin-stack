a7d9d849cd50234ea9e30814e266e468d99e5aa5
Gamer100309 <139044183+Gamer100309@users.noreply.github.com>
2026-07-18 22:06:06 +0200

    Delete ytdlp2strm directory

--- ytdlp2strm/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Utils package for ytdlp2STRM
-"""
-from .sanitize import sanitize, sanitize_path
-from .episode_numbering import format_episode_title
-
-__all__ = ['sanitize', 'sanitize_path', 'format_episode_title']
--- ytdlp2strm/version.py
+++
@@ -1,9 +0,0 @@
-"""Single source of truth for the ytdlp2STRM application version.
-
-Bump this string on every release following SemVer (MAJOR.MINOR.PATCH):
-- MAJOR: incompatible / breaking changes.
-- MINOR: backwards-compatible new features.
-- PATCH: backwards-compatible bug fixes.
-"""
-
-__version__ = "1.1.3"
